25e9cddfa53d4e17ba210ae836b92354cc305a93
ravi teja <raviteja080@gmail.com>
2020-08-09 13:23:42 +0530

    Add files via upload

---
+++ hacker rank problem set 1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 16:39:47 2020
+
+@author: ravit
+"""
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+x = pow(a,b)
+y = pow(c,d)
+print(x+y)
---
+++ hacker rank problem.ipynb
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 12:26:58 2020
+
+@author: ravit
+"""
+
+x= int(input())
+y= int(input())
+q, r = divmod (x,y)
+print (q)
+print (r)
+print (q,r)
